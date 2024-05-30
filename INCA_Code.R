11c2a2ccc0de1b1e6e96d5d224cf4c0dd5fdefe4
Kiera ODonnell <klo30@duke.edu>
2024-05-30 15:22:44 -0400

    Setting up the code with the INCA description

---
+++ INCA_Code.R
@@ -0,0 +1,18 @@
+###### INCA SUNS
+# We aim to take a closer look at the methods typically used to account for human 
+# benefits of NBFs. In the most simplistic terms, a buffer is taken around the 
+# project and the number of people found within that buffer are benefited.
+# However, different sizes of buffers, different methods of setting the buffer,
+# and different metrics of counting humans benefited have been used to define 
+# social benefit from NBF.
+
+# 3 different buffers (300m, 500m, and 1,000m) were produced for each NBS project
+# from the TNC SUNS portfolio. The intersection between each of these buffers and
+# 2 feet of SLR and the intersection between each of these buffers and FEMA's 
+# special flood hazard areas (SFHA) were created. This results in 9 areas of 
+# potential human benefit. The sum of people in each of these areas was taken 
+# using 1) night time landscan imagery (30m resolution), 2) census block 
+# populations intersected with the 9 areas, 3) total buildings within the 
+# buffers using the parcel tax database, and 4) the total number of residential 
+# serving parcels. Residential serving parcels included, residential parcels,
+# public parks, schools, etc.
